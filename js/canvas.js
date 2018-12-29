f87678d89c28c9f59d887f765c8edc0b21d2a82b
cbruhns <cameron.bruhns@gmail.com>
2018-12-28 22:53:27 -0500

    Working Moving Dots From Tutorial

---
+++ js/canvas.js
@@ -0,0 +1,66 @@
+
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext('2d');
+
+
+
+function Circle(x,y,dx,dy,rad){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.rad = rad;
+
+    this.draw = function (){
+        c.beginPath();
+        c.arc(this.x, this.y, this.rad, 0, Math.PI*2, false);
+        c.fill();
+        c.fillStyle = 'red';
+    }
+
+    this.update = function(){
+        if (this.x + this.rad > window.innerWidth || this.x - this.rad < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.rad > window.innerHeight || this.y - this.rad < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+
+}
+
+var circleArray = [];
+for (var i = 0; i < 100; i++) {
+    var x = Math.random() * (window.innerWidth - radius * 2) + radius;
+    var y = Math.random() * (window.innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 4;
+    var dy = (Math.random() - 0.5) * 4;
+    var radius = Math.floor(Math.random() * 10)+3;
+    circleArray.push(new Circle(x,y,dx,dy,radius));
+}
+console.log(circleArray);
+
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+
+}
+animate();
+
+
+window.addEventListener('resize', function(){
+
+});
